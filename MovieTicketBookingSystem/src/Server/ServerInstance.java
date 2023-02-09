073d89aa503487ea19fde5775fbba1c6d10bb5fa
Ishan-3213 <ishan18112000@gmail.com>
2023-02-09 12:41:16 -0500

    bugfix

--- MovieTicketBookingSystem/src/Server/ServerInstance.java
+++ MovieTicketBookingSystem/src/Server/ServerInstance.java
@@ -9,4 +9,5 @@ public class ServerInstance {
         Server server_instance_atwater = new Server(8001, "ATW", args);
         Server server_instance_verdnum = new Server(8002, "VER", args);
         Server server_instance_outremont = new Server(8003, "OUT", args);
+    }
 }
